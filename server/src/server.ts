2ee3197c2078b57afb57d2920538170eb16a8bff
ElinEunjung <eunjungp615@gmail.com>
2025-08-04 23:38:11 +0200

    feat: create helper module (db.ts) to clean up server.ts - Extracted MongoDB connection logic into a reusable helper module - Updated server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,26 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db";
 
 dotenv.config();
 
 const app= express();
 const PORT = process.env.PORT || 3000;
-const uri = process.env.MONGO_URI || '';
 
+// Middleware
 app.use(cors());
 app.use(express.json())
 
+// Connect to MongoDB
+connectDB()
+
+// Routes
 app.get('/', (_req, res) => {
     res.send('API is running!');
 });
 
-// Connect to MongoDB and start server
-mongoose.connect(uri, {})
-    .then(() => {
-        console.log('MongoDB connected successfully');
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error(`MongoDB connection failed:`, err);
-    });
+// start server
+app.listen(PORT, () => {
+    console.log(`🚀Server running on http://localhost:${PORT}`);
+});
