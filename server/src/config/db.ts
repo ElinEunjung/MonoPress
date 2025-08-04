175dec72019dfb5a8503c29058f0e6eee43b74c0
ElinEunjung <eunjungp615@gmail.com>
2025-08-05 00:34:20 +0200

    fix(package.json, tsconfig.json): update compiler option

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-export const connectDB = async (): Promise<void> => {
+export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI || '');
         console.log(`✅ MongoDB connected: ${conn.connection.host}`);
