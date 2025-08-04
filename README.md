ac05caa331e49a15d028e9d47f0a8f356d0a49a2
ElinEunjung <eunjungp615@gmail.com>
2025-08-05 01:22:46 +0200

    chore: test prettier

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # VibePress
+
+[GitHub][(https://github.com/kristiania-pg6301-2024/pg6301-2025-konte-ElinEunjung)]
+[Heroku][(add-link-here)]
+[Test Rapport][(add-link-here)]
+
+#
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,22 +5,22 @@ import { connectDB } from "./config/db";
 
 dotenv.config();
 
-const app= express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // Connect to MongoDB
-connectDB()
+connectDB();
 
 // Routes
-app.get('/', (_req, res) => {
-    res.send('API is running!');
+app.get("/", (_req, res) => {
+  res.send("API is running!");
 });
 
 // start server
 app.listen(PORT, () => {
-    console.log(`🚀Server running on http://localhost:${PORT}`);
+  console.log(`🚀Server running on http://localhost:${PORT}`);
 });
