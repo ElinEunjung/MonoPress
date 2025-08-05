089ac4e15f03bff76c9fd8980478b1412ac6e66f
ElinEunjung <eunjungp615@gmail.com>
2025-08-06 01:10:27 +0200

    WIP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
 export const App = () => {
+  useEffect(() => {
+    fetch("http://localhost:5000/api/hello")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
   return (
     <>
       <h1>Welcome to bvla blal bla </h1>
