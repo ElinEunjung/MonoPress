adf4da3542f0e54475a5ca9a2e2bd11b623be160
ElinEunjung <eunjungp615@gmail.com>
2025-08-06 01:29:31 +0200

    WIP

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(express.json());
 // Serve static files from the Vite build directory
