51dfb5bdba095b1704f9ed161e7fa03dce1f412b
ElinEunjung <eunjungp615@gmail.com>
2025-08-06 01:15:04 +0200

    WIP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export const App = () => {
   useEffect(() => {
-    fetch("https://mono-press-5a039da642a5.herokuapp.com:5000/api/hello")
+    fetch("https://mono-press-5a039da642a5.herokuapp.com/api/hello")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
